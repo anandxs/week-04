5667be2a664e6465862f49d6ba7f7f5ec267b29e
Anand S <anandsatheeshk@gmail.com>
2023-06-08 22:06:03 +0530

    Add project files.

---
+++ DoublyLinkedListProgram/Program.cs
@@ -0,0 +1,151 @@
+﻿namespace DoublyLinkedListProgram
+{
+	class Node
+	{
+		public int val;
+		public Node? prev;
+		public Node? next;
+
+		public Node (int val)
+		{
+			this.val = val;
+			prev = next = null;
+		}
+	}
+	class DoublyLinkedList
+	{
+		public Node? head;
+		public Node? tail;
+
+        public DoublyLinkedList()
+        {
+			head = null;
+			tail = null;
+		}
+
+		public void AddToEnd(int val)
+		{
+			Node node = new Node(val);
+
+			if (head == null)
+			{
+				head = tail = node;
+			}
+			else
+			{
+				tail.next = node;
+				node.prev = tail;
+				tail = node;
+			}
+		}
+
+		public void AddToStart(int val)
+		{
+			Node node = new Node(val);
+
+			if (head == null)
+			{
+				head = tail = node;
+			}
+			else
+			{
+				node.next = head;
+				head.prev = node;
+				head = node;
+			}
+		}
+
+		public int RemoveFirst()
+		{
+			if (head == null)
+				throw new Exception("Empty string");
+
+			int result = head.val;
+			if (head == tail)
+				head = tail = null;
+			else
+			{
+				Node temp = head;
+				head = head.next;
+				head.prev = null;
+				temp.next = null;
+			}
+			
+			return result;
+		}
+
+		public int RemoveLast()
+		{
+			if (tail == null)
+				throw new Exception("Empty string!");
+
+			int result = tail.val;
+
+			if (head == tail)
+				head = tail = null;
+			else
+			{
+				Node temp = tail;
+				tail = tail.prev;
+				tail.next = null;
+				temp.prev = null;
+			}
+
+			return result;
+		}
+
+		public void Remove(int val)
+		{
+			if (Contains(val))
+			{
+
+			}
+			else
+			{
+                Console.WriteLine($"Cannot find {val}");
+            }
+		}
+
+		public bool Contains(int val)
+		{
+			Node curr = head;
+
+			while(curr != null)
+			{
+				if (curr.val == val)
+					return true;
+
+				curr = curr.next;
+			}
+
+			return false;
+		}
+
+		public void Print()
+		{
+			Node curr = head;
+			while (curr != null)
+			{
+                Console.Write(curr.val + " ");
+				curr = curr.next;
+            }
+		}
+
+		public void ReversePrint()
+		{
+			Node curr = tail;
+			while (curr != null)
+			{
+                Console.Write(curr.val + " ");
+                curr = curr.prev;
+			}
+		}
+    }
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			DoublyLinkedList list = new DoublyLinkedList();
+        }
+	}
+}
---
+++ LinkedListProgram/Program.cs
@@ -0,0 +1,304 @@
+﻿namespace LinkedListProgram
+{
+	class Node
+	{
+		public int val;
+		public Node? next;
+
+		public Node(int val)
+		{
+			this.val = val;
+			this.next = null;
+		}
+	}
+	class SinglyLinkedList
+	{
+		public Node? head;
+		public Node? tail;
+
+		public SinglyLinkedList()
+		{
+			head = tail = null;
+		}
+
+		public void AddToEnd(int val)
+		{
+			Node temp = new Node(val);
+
+			if (tail == null)
+			{
+				head = tail = temp;
+			}
+			else
+			{
+				tail.next = temp;
+				tail = temp;
+			}
+		}
+
+		public void AddToStart(int val)
+		{
+			Node temp = new Node(val);
+
+			if (head == null)
+			{
+				head = tail = temp;
+			}
+			else
+			{
+				temp.next = head;
+				head = temp;
+			}
+		}
+
+		public void AddAfter(int newVal, int checkVal)
+		{
+			if (Contains(checkVal))
+			{
+				Node node = new Node(newVal);
+				Node curr = head;
+
+				while (curr != null)
+				{
+					if (curr.val == checkVal)
+					{
+						node.next = curr.next;
+						curr.next = node;
+						break;
+					}
+
+					curr = curr.next;
+				}
+			}
+			else
+			{
+                Console.WriteLine($"{checkVal} does not exist in list");
+            }
+		}
+
+		public void AddBefore(int newVal, int checkVal)
+		{
+			if (Contains(checkVal))
+			{
+				if (head.val == checkVal)
+				{
+					AddToStart(newVal);
+				}
+				else
+				{
+					Node curr = head;
+					while(curr.next != null)
+					{
+						if (curr.next.val == checkVal)
+						{
+							AddAfter(newVal, curr.val);
+							break;
+						}
+
+						curr = curr.next;
+					}
+				}
+			}
+			else
+			{
+                Console.WriteLine($"{checkVal} does not exits in list");
+            }
+		}
+
+		public int RemoveFirst()
+		{
+			if (head == null)
+				throw new Exception("Empty list");
+
+			Node temp = head;
+			int result = temp.val;
+			head = head.next;
+			temp.next = null;
+
+			if (head == null)
+				tail = null;
+
+			return result;
+		}
+
+		public int RemoveLast()
+		{
+			if (tail == null)
+			{
+				throw new Exception("Empty list");
+			}
+
+			int result = tail.val;
+			if (head == tail)
+			{
+				head = tail = null;
+			}
+			else
+			{
+				Node temp = head;
+				while (temp.next.next != null)
+				{
+					temp = temp.next;
+				}
+				tail = temp;
+				temp.next = null;
+			}
+
+			return result;
+		}
+
+		public void Remove(int val)
+		{
+			if (Contains(val))
+			{
+				Node del = Find(val);
+
+				if (del == head)
+					RemoveFirst();
+				else if (del == tail)
+					RemoveLast();
+				else
+				{
+					Node prev = head;
+					Node curr = head.next;
+					Node next = curr.next;
+
+					while (curr.next != null)
+					{
+						if (curr.val == val)
+						{
+							prev.next = next;
+							curr.next = null;
+						}
+						prev = curr;
+						curr = next;
+						next = next.next;
+					}
+				}
+			}
+			else
+			{
+                Console.WriteLine($"{val} not in list");
+            }
+		}
+
+		public void RemoveDuplicates()
+		{
+			Node prev, curr, next;
+			curr = head;
+
+			while (curr.next != null)
+			{
+				next = curr.next;
+
+				if (curr.val == next.val)
+				{
+					curr.next = next.next;
+					next.next = null;
+				}
+				else
+				{
+					curr = curr.next;
+				}
+			}
+		}
+
+		public bool Contains(int val)
+		{
+			if (head == null)
+				return false;
+
+			Node temp = head;
+
+			while (temp != null)
+			{
+				if (temp.val == val)
+					return true;
+
+				temp = temp.next;
+			}
+
+			return false;
+		}
+
+		public Node Find(int val)
+		{
+			if (head == null)
+				return null;
+
+			Node temp = head;
+
+			while (temp != null)
+			{
+				if (temp.val == val)
+					return temp;
+
+				temp = temp.next;
+			}
+
+			return null;
+		}
+
+		public void Clear()
+		{
+			while (head != null)
+			{
+				RemoveLast();
+			}
+		}
+
+		public void ArrayToSinglyLinkedList(int[] arr)
+		{
+			if (head != null)
+			{
+				Clear();
+			}
+
+			foreach (int number in arr)
+			{
+				AddToEnd(number);
+			}
+		}
+
+		public void Print()
+		{
+			Node temp = head;
+
+			while (temp != null)
+			{
+				Console.Write(temp.val + " ");
+				temp = temp.next;
+			}
+		}
+
+		public void ReversePrint()
+		{
+			if (head == null)
+				Print();
+			else
+				ReversePrintHelper(head);
+		}
+
+		private void ReversePrintHelper(Node? head)
+		{
+			if (head.next == null)
+			{
+                Console.Write(head.val + " ");
+            }
+			else
+			{
+				ReversePrintHelper(head.next);
+				Console.Write(head.val + " ");
+			}
+
+        }
+
+	}
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+        }
+	}
+}
